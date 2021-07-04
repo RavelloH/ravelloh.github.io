81f66af24ea1ed8ad9129f3e7b34da0b978d2f27
RavelloH <dr_hamilton@foxmail.com>
2021-07-04 18:34:10 +0800

    message

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
-<li><a href='./'><span class='iconfontsmall icon-home'></span> Home</a></li>
-<li><a href='apps/'><span class='iconfontsmall icon-app'></span> Apps</a></li>
-<li><a href='articles'><span class='iconfontsmall icon-article'></span> Articles</a></li>
-<li><a href='tag/'><span class='iconfontsmall icon-tag'></span> Tag</a></li>
-<li><a href='classification/'><span class='iconfontsmall icon-classification'></span> Classification</a>
-<li><a href='friends/'><span class='iconfontsmall icon-share'></span> Friends</a></li>
-<li><a href='about/'><span class='iconfontsmall icon-about'></span> About</a></li>
+document.writeln("<li><a href=\'/\'><span class=\'iconfontsmall icon-home\'></span> Home</a></li>");
+document.writeln("<li><a href=\'/apps/\'><span class=\'iconfontsmall icon-app\'></span> Apps</a></li>");
+document.writeln("<li><a href=\'/articles\'><span class=\'iconfontsmall icon-article\'></span> Articles</a></li>");
+document.writeln("<li><a href=\'/tag/\'><span class=\'iconfontsmall icon-tag\'></span> Tag</a></li>");
+document.writeln("<li><a href=\'/classification/\'><span class=\'iconfontsmall icon-classification\'></span> Classification</a>");
+document.writeln("<li><a href=\'/friends/\'><span class=\'iconfontsmall icon-friend\'></span> Friends</a></li>");
+document.writeln("<li><a href=\'/about/\'><span class=\'iconfontsmall icon-about\'></span> About</a></li>");
