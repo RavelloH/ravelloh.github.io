21855c3d5c8a108e5d33e545a9ad67a4739bb5d0
RavelloH <hyh20060327@qq.com>
2021-07-06 15:25:23 +0800

    fix the menu

--- js/menu.js
+++ js/menu.js
@@ -5,3 +5,4 @@ document.writeln("<li><a href=\'/tag/\'><span class=\'iconfontsmall icon-tag\'><
 document.writeln("<li><a href=\'/classification/\'><span class=\'iconfontsmall icon-classification\'></span> Classification</a>");
 document.writeln("<li><a href=\'/friends/\'><span class=\'iconfontsmall icon-friend\'></span> Friends</a></li>");
 document.writeln("<li><a href=\'/about/\'><span class=\'iconfontsmall icon-about\'></span> About</a></li>");
+document.writeln("<b><br><br>Copyright © 2019 - <b id=\'year\'></b> RavelloH.<br> All rights reserved.</b>");
