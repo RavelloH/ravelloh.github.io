9728d0382e54aee7392b9abf205b48304619dd35
RavelloH <hyh20060327@qq.com>
2021-07-06 15:25:23 +0800

    fix the menu

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ const menuToggle = document.querySelector('.toggle');
         menuToggle.classList.toggle('active');
         showcase.classList.toggle('active');
       })
+
+      Date.prototype.format = function (fmt) {
+        var o = {
+        };
+        if (/(y+)/.test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        }
+        return fmt;
+    }
+    var now = new Date();
+    var nowStr = now.format("yyyy");
+    document.getElementById("year").innerHTML = new Date().format("yyyy");
