7df07b4f0a075a770b34f0ccaaea270e8e49b3e4
RavelloH <hyh20060327@qq.com>
2021-07-07 22:57:41 +0800

    格式规范化

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 const menuToggle = document.querySelector('.toggle');
-      const showcase = document.querySelector('.showcase');
+const showcase = document.querySelector('.showcase');
 
-      menuToggle.addEventListener('click', () => {
-        menuToggle.classList.toggle('active');
-        showcase.classList.toggle('active');
-      })
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+})
 
-      Date.prototype.format = function (fmt) {
-        var o = {
-        };
-        if (/(y+)/.test(fmt)) {
-            fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-        }
-        return fmt;
-    }
-    var now = new Date();
-    var nowStr = now.format("yyyy");
-    document.getElementById("year").innerHTML = new Date().format("yyyy");
+Date.prototype.format = function (fmt) {
+  var o = {};
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+  }
+  return fmt;
+}
+var now = new Date();
+var nowStr = now.format("yyyy");
+document.getElementById("year").innerHTML = new Date().format("yyyy");
