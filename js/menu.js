8922809ac305962edbdd44994ad5f056fedd0b26
RavelloH <hyh20060327@qq.com>
2021-07-21 13:24:23 +0800

    add works

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 document.writeln("<li><a href=\'/\'><span class=\'iconfontsmall icon-home\'></span> Home</a></li>");
-document.writeln("<li><a href=\'/apps/\'><span class=\'iconfontsmall icon-app\'></span> Apps</a></li>");
+document.writeln("<li><a href=\'/works/\'><span class=\'iconfontsmall icon-app\'></span> Works</a></li>");
 document.writeln("<li><a href=\'/articles\'><span class=\'iconfontsmall icon-article\'></span> Articles</a></li>");
 document.writeln("<li><a href=\'/tag/\'><span class=\'iconfontsmall icon-tag\'></span> Tag</a></li>");
 document.writeln("<li><a href=\'/classification/\'><span class=\'iconfontsmall icon-classification\'></span> Classification</a>");
