9d83c12837d00879b2fdc704f8a64104df0fc3e8
RavelloH <68409330+RavelloH@users.noreply.github.com>
2022-03-25 11:58:01 +0800

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
-document.writeln("<li><a href=\'/\'><span class=\'iconfontsmall icon-home\'></span> Home</a></li>");
-document.writeln("<li><a href=\'/works/\'><span class=\'iconfontsmall icon-app\'></span> Works</a></li>");
-document.writeln("<li><a href=\'/articles\'><span class=\'iconfontsmall icon-article\'></span> Articles</a></li>");
-document.writeln("<li><a href=\'/tag/\'><span class=\'iconfontsmall icon-tag\'></span> Tag</a></li>");
-document.writeln("<li><a href=\'/classification/\'><span class=\'iconfontsmall icon-classification\'></span> Classification</a>");
-document.writeln("<li><a href=\'/friends/\'><span class=\'iconfontsmall icon-friend\'></span> Friends</a></li>");
-document.writeln("<li><a href=\'/about/\'><span class=\'iconfontsmall icon-about\'></span> About</a></li>");
+document.writeln("<li style=\"--i: 0\"><a href=\'/\'><span class=\'iconfontsmall icon-home\'></span> Home</a></li>");
+document.writeln("<li style=\"--i: 1\"><a href=\'/works/\'><span class=\'iconfontsmall icon-app\'></span> Works</a></li>");
+document.writeln("<li style=\"--i: 2\"><a href=\'/articles\'><span class=\'iconfontsmall icon-article\'></span> Articles</a></li>");
+document.writeln("<li style=\"--i: 3\"><a href=\'/tag/\'><span class=\'iconfontsmall icon-tag\'></span> Tag</a></li>");
+document.writeln("<li style=\"--i: 4\"><a href=\'/classification/\'><span class=\'iconfontsmall icon-classification\'></span> Classification</a>");
+document.writeln("<li style=\"--i: 5\"><a href=\'/friends/\'><span class=\'iconfontsmall icon-friend\'></span> Friends</a></li>");
+document.writeln("<li style=\"--i: 6\"><a href=\'/about/\'><span class=\'iconfontsmall icon-about\'></span> About</a></li>");
 document.writeln("<b><br><br>Copyright © 2019 - <b id=\'year\'></b> RavelloH.<br> All rights reserved.</b>");
