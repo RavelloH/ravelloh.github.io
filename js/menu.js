fdfa34a000065e1128d7ccad1607f6f7a9484b41
RavelloH <hyh20060327@qq.com>
2022-07-08 23:04:02 +0800

    新文章归档

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,8 @@
-document.writeln("<li style=\"--i: 0\"><a href=\'/\'><span class=\'iconfontsmall icon-home\'></span> Home</a></li>");
-document.writeln("<li style=\"--i: 1\"><a href=\'/works/\'><span class=\'iconfontsmall icon-app\'></span> Works</a></li>");
-document.writeln("<li style=\"--i: 2\"><a href=\'/articles\'><span class=\'iconfontsmall icon-article\'></span> Articles</a></li>");
-document.writeln("<li style=\"--i: 3\"><a href=\'/tag/\'><span class=\'iconfontsmall icon-tag\'></span> Tag</a></li>");
-document.writeln("<li style=\"--i: 4\"><a href=\'/classification/\'><span class=\'iconfontsmall icon-classification\'></span> Classification</a>");
-document.writeln("<li style=\"--i: 5\"><a href=\'/friends/\'><span class=\'iconfontsmall icon-friend\'></span> Friends</a></li>");
-document.writeln("<li style=\"--i: 6\"><a href=\'/about/\'><span class=\'iconfontsmall icon-about\'></span> About</a></li>");
+document.writeln("<li style=\"--i: 0\"><a href=\'/\'><span class=\'iconfontsmall icon-home\'></span> HOME</a></li>");
+document.writeln("<li style=\"--i: 1\"><a href=\'/works/\'><span class=\'iconfontsmall icon-app\'></span> WORKS</a></li>");
+document.writeln("<li style=\"--i: 2\"><a href=\'/articles\'><span class=\'iconfontsmall icon-article\'></span> ARTICLES</a></li>");
+document.writeln("<li style=\"--i: 3\"><a href=\'/tag/\'><span class=\'iconfontsmall icon-tag\'></span> TAG</a></li>");
+document.writeln("<li style=\"--i: 4\"><a href=\'/classification/\'><span class=\'iconfontsmall icon-classification\'></span> CLASSIFICATION</a>");
+document.writeln("<li style=\"--i: 5\"><a href=\'/friends/\'><span class=\'iconfontsmall icon-friend\'></span> FRIENDS</a></li>");
+document.writeln("<li style=\"--i: 6\"><a href=\'/about/\'><span class=\'iconfontsmall icon-about\'></span> ABOUT</a></li>");
 document.writeln("<b><br><br>Copyright © 2019 - <b id=\'year\'></b> RavelloH.<br> All rights reserved.</b>");
