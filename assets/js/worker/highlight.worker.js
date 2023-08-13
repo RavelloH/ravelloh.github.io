e820a5d62b56e8d16b5e2b7cadeda5f7edcb1170
RavelloH <hyh20060327@qq.com>
2023-08-13 16:55:44 +0800

    Opti. 优化代码高亮逻辑

--- assets/js/worker/highlight.worker.js
+++ assets/js/worker/highlight.worker.js
@@ -1,4 +1,17 @@
 importScripts('../lib/highlight.min.js');
+function HTMLEncode(str) {
+    var s = '';
+    if (str.length == 0) return '';
+    s = str.replace(/&/g, '&amp;');
+    s = s.replace(/</g, '&lt;');
+    s = s.replace(/>/g, '&gt;');
+    s = s.replace(/ /g, '&nbsp;');
+    s = s.replace(/\'/g, '&#39;');
+    s = s.replace(/\"/g, '&quot;');
+    s = s.replace(/\n/g, '<br/>');
+    return s;
+}
+
 function HTMLDecode(str) {
     var s = '';
     if (str.length == 0) return '';
@@ -14,15 +27,21 @@ function HTMLDecode(str) {
 
 onmessage = (event) => {
     if (/<span(([\s\S])*?)<\/span>/.test(event.data)) {
-        var originCode = HTMLDecode(event.data)
+        var originCode = event.data
             .split('\n')
             .map((e) => ['', ...e.split('')].reduce((p, n) => (!p && n === ' ' ? '' : p + n)))
             .map((e) => e.slice(6, -7))
             .join('\n');
-        var result = self.hljs.highlightAuto(originCode);
+        var result = self.hljs.highlightAuto(HTMLDecode(HTMLEncode(HTMLDecode(originCode))));
         var processedCode = result.value
             .split('\n')
-            .map((e) => '<span>' + e + '</span>')
+            .map((e) => {
+                if (e.includes('<span')) {
+                    return '<span>' + e + '</span>';
+                } else {
+                    return '<span>' + e.replace(/<br\/>/g, '\n').replace(/\n/g, '') + '</span>';
+                }
+            })
             .join('\n');
         postMessage(processedCode.substring(0, processedCode.length - 13));
     } else {
